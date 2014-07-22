7f1c770635c69c77d620396ccc9f65e17da00473
Stephen Salinas <shsalinas2012@gmail.com>
2014-07-22 16:41:34 -0400

    home image and other assets

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -2,12 +2,12 @@ $(function() {
     var window_height = $(window).height(),
        content_height = window_height - 200;
     $('.facebookfeed').height(content_height);
-    $('.leftsidecol').height(content_height + 50);
+    $('.leftsidecol').height(content_height);
 });
 
 $( window ).resize(function() {
     var window_height = $(window).height(),
         content_height = window_height - 200;
     $('.facebookfeed').height(content_height);
-    $('.leftsidecol').height(content_height + 50);
+    $('.leftsidecol').height(content_height);
 });
